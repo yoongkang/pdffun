46ffde3b7bdd5f28da22ca625090237b8a025b24
Yoong Kang Lim <yoongkang.lim@gmail.com>
2018-03-07 17:50:26 +1100

    stuff

--- home/static/home/fill.py
+++ home/static/home/fill.py
@@ -2,6 +2,11 @@ from pdfjinja import PdfJinja
 
 
 mypdf = PdfJinja('sydjango3.pdf')
-pdfout = mypdf({'name': 'yoongie', 'sydney': True, 'address': 'test address', 'language': 'javascript'})
+pdfout = mypdf({
+    'name': 'yoongie',
+    'sydney': True,
+    'address': 'test address',
+    'language': 'javascript'
+})
 with open('filled.pdf', 'wb') as f:
     pdfout.write(f)
